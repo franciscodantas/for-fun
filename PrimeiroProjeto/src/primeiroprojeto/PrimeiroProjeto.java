f6774020b415e60294a3d17fb093322d0700637c
Francisco Antonio Dantas de Sousa <96065182+franciscodantas@users.noreply.github.com>
2021-12-14 14:12:40 -0300

    Add files via upload

---
+++ PrimeiroProjeto/src/primeiroprojeto/PrimeiroProjeto.java
@@ -0,0 +1,13 @@
+package primeiroprojeto;
+
+import java.util.Date;
+import java.util.Locale;
+
+public class PrimeiroProjeto {
+    public static void main(String[] args) {
+        Locale idioma;
+        idioma = Locale.getDefault();
+        System.out.println("Seu sistema está em");
+        System.out.println(idioma);
+    }
+}
